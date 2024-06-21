0e03b87c65407519e207ee131eb821d7eb820ae5
uygarpolat <uygarpolat@gmail.com>
2024-06-21 18:40:08 +0300

    files

--- philo.c
+++ philo.c
@@ -6,7 +6,7 @@
 /*   By: upolat <upolat@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/21 00:31:07 by upolat            #+#    #+#             */
-/*   Updated: 2024/06/21 00:54:26 by upolat           ###   ########.fr       */
+/*   Updated: 2024/06/21 18:39:27 by upolat           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,43 +15,28 @@
 #include <stdlib.h>
 #include <sys/time.h>
 
-// Function to print the current time
-void print_timestamp(const char *msg)
+void	*thread_function(void *arg)
 {
-	struct timeval tv;
+	char			*str;
+	struct timeval	tv;
+
 	gettimeofday(&tv, NULL);
-	printf("%s: %ld.%06d\n", msg, tv.tv_sec, tv.tv_usec);
+	str = (char *) arg;
+	printf("%s: %ld.%06d\n", str, tv.tv_sec, tv.tv_usec);
+	return (NULL);
 }
 
-// Function to be executed by the thread
-void *thread_function(void *arg)
+int	main(void)
 {
-	(void)arg;
-	print_timestamp("This gets printed first: This is the thread");
-	return NULL;
-}
+	pthread_t	thread;
+	char		*str;
 
-int main(void)
-{
-	pthread_t thread;
-
-	// Create a new thread
-	if (pthread_create(&thread, NULL, thread_function, NULL) != 0)
-	{
-		perror("pthread_create");
-		return 1;
-	}
-
-	print_timestamp("Will this get printed out first or second? Should be second");
-
+	str = "Time of execution:";
+	if (pthread_create(&thread, NULL, thread_function, (void *)str) != 0)
+		return (perror("pthread_create"), 1);
+	thread_function((void *)str);
 	if (pthread_join(thread, NULL) != 0)
-	{
-		perror("pthread_join");
-		return 1;
-	}
-
-	print_timestamp("This is the main thread. This will for sure get printed out the last");
-
-	return 0;
+		return (perror("pthread_join"), 1);
+	thread_function((void *)str);
+	return (0);
 }
-
