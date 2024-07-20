e033952277bc5be95c7e7cfb56fa8b792b3ce95a
uygarpolat <uygarpolat@gmail.com>
2024-07-20 03:49:05 +0300

    overseer.c

--- src/mandatory/overseer.c
+++ src/mandatory/overseer.c
@@ -6,7 +6,7 @@
 /*   By: upolat <upolat@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/18 00:35:06 by upolat            #+#    #+#             */
-/*   Updated: 2024/07/19 17:42:29 by upolat           ###   ########.fr       */
+/*   Updated: 2024/07/20 02:58:40 by upolat           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,6 +40,38 @@ static int	is_there_death(t_overseer *o)
 {
 	int		i;
 	size_t	time_since_start_of_meal;
+	size_t	time_since_start_of_sim;
+
+	i = -1;
+	while (++i < o->number_of_philos)
+	{
+		time_since_start_of_sim = what_time_is_it() - o->philos[i].last_eating_time;
+		pthread_mutex_lock(&o->time_mutex[i]);
+		time_since_start_of_meal = what_time_is_it() - o->philos[i].last_eating_time2;
+		pthread_mutex_unlock(&o->time_mutex[i]);
+		pthread_mutex_lock(&o->write_mutex[i]);
+		if ((!o->philos[i].ate && time_since_start_of_sim >= o->time_to_die) || time_since_start_of_meal >= o->time_to_die)
+		{
+			pthread_mutex_unlock(&o->write_mutex[i]);
+			pthread_mutex_lock(&o->death_mutex);
+			o->death = 2;
+			pthread_mutex_unlock(&o->death_mutex);
+			if (time_since_start_of_meal < time_since_start_of_sim)
+				time_since_start_of_meal = time_since_start_of_sim;
+			pthread_mutex_lock(&o->print_mutex);
+			printf("%zu %d died\n", time_since_start_of_meal, i + 1);
+			pthread_mutex_unlock(&o->print_mutex);
+			return (1);
+		}
+		pthread_mutex_unlock(&o->write_mutex[i]);
+	}
+	return (0);
+}
+/*
+static int	is_there_death2(t_overseer *o)
+{
+	int		i;
+	size_t	time_since_start_of_meal;
 
 	i = -1;
 	while (++i < o->number_of_philos)
@@ -55,14 +87,14 @@ static int	is_there_death(t_overseer *o)
 			pthread_mutex_unlock(&o->death_mutex);
 			pthread_mutex_lock(&o->print_mutex);
 			printf("%zu %d died\n", what_time_is_it()
-				- o->philos[i].last_eating_time, i + 1);
+				- o->philos[i].last_eating_time2, i + 1); // when you change "time2" to "time", it works! Unless it isn't the first time philo eats, then it doesn't work.
 			pthread_mutex_unlock(&o->print_mutex);
 			return (1);
 		}
 	}
 	return (0);
 }
-
+*/
 void	ft_overseer(t_overseer *o)
 {
 	while (1)
